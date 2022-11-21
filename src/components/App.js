7dd749f3d8f8f4fd60617be29f0017ab09612a0f
nuriya <nuriya_a@yahoo.com>
2022-11-21 17:54:42 -0500

    feature footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import Header from "./Header";
 import Main from "./Main";
-import Footer from "./Footer";
+import Footer from "./Footer/Footer";
 
 /* -------------------------------------------------------------------------- */
 /*                                function App                                */
