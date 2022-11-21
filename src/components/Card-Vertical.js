e5c0da788bc69be889b3ea9630ee200f4102fb44
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-11-21 15:23:19 -0800

    initial commit feat/vert-slide

---
+++ src/components/Card-Vertical.js
@@ -0,0 +1,5 @@
+//components/Card-Vertical
+/* --------------------------------- imports -------------------------------- */
+import styled from "styled-components";
+
+//Create a CardWrapper component that will render a 
