c8c1b3163fc2b6a06b222368edb451e03685eb24
JulieBodette <jr3python@gmail.com>
2022-11-23 16:44:18 -0500

    there is a box
    soon the box will be animated...but at least it exists :)

---
+++ src/Animation/Box1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Box1() {
+return(
+    <div className="box-container">
+        <div className="box">
+            
+        </div>
+
+    </div>
+    )
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 /* --------------------------------- imports -------------------------------- */
-import React from "react";
+import React from 'react';
 
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import Box1 from '../Animation/Box1.jsx';
+import '../Animation/Box1.css';
 
 /* -------------------------------------------------------------------------- */
 /*                                function App                                */
 /* -------------------------------------------------------------------------- */
-function App() { 
-
+function App() {
   return (
     <div className="root">
       <div className="page">
         <Header />
+        <Box1 />
         <Main />
         <Footer />
       </div>
