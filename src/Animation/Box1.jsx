0fe144bb5bbc91a94c98f66b822a54dd3b6d0db9
JulieBodette <jr3python@gmail.com>
2022-11-23 22:16:35 -0500

    box goes down
    box now goes down and the opacity changes

--- src/Animation/Box1.jsx
+++ src/Animation/Box1.jsx
@@ -4,9 +4,10 @@ import {motion} from "framer-motion"
 export default function Box1() {
 return(
     <div className="box-container">
-        <motion.div className="box" animate={{
-        x: 500
-        }}>
+        <motion.div className="box" 
+        initial ={{opacity: 0}}
+        animate={{ y: 500, opacity: 1 }} 
+        transition={{ duration: 2 }}>
 
         </motion.div>
 
