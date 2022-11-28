defb72ce3cd0d92341a6b901a9ed094ff429720d
JulieBodette <jr3python@gmail.com>
2022-11-27 22:41:38 -0500

    2 rows of cubes
    I am manually setting the delay
    there is probably a  better way, but this works...

--- src/Animation/Box1.jsx
+++ src/Animation/Box1.jsx
@@ -13,16 +13,31 @@ export default function Box1() {
 
 return(
     <div>
+{/*second from bottom row*/}
+<motion.div className="box-container">
+        <motion.div className="box-dark-gray" initial="start" animate="end" variants={littleCube} transition={{delay: 1.4, duration: 2 }}>
+        </motion.div>
+        <motion.div className="box-dark-gray" initial="start" animate="end" variants={littleCube} transition={{ delay: 1.2, duration: 2 }}>
+        </motion.div>
+        <motion.div className="box-blue" initial="start" animate="end" variants={littleCube} transition={{ delay: 1, duration: 2 }}>
+        </motion.div>
+        <motion.div className="box-light-gray" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.8, duration: 2 }}>
+        </motion.div>
+    </motion.div>
+
+
+{/*bottom row*/}
     <motion.div className="box-container">
-        <motion.div className="box-blue" initial="start" animate="end" variants={littleCube} transition={{ duration: 2 }}>
+        <motion.div className="box-light-gray" initial="start" animate="end" variants={littleCube} transition={{ duration: 2 }}>
         </motion.div>
         <motion.div className="box-orange" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.2, duration: 2 }}>
         </motion.div>
-        <motion.div className="box-light-gray" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.4, duration: 2 }}>
+        <motion.div className="box-dark-gray" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.4, duration: 2 }}>
         </motion.div>
-        <motion.div className="box-dark-gray" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.6, duration: 2 }}>
+        <motion.div className="box-blue" initial="start" animate="end" variants={littleCube} transition={{ delay: 0.6, duration: 2 }}>
         </motion.div>
     </motion.div>
+
 
 
 
