51e8244f67fcd1e9b93bcf352e7edefe0715aee3
JulieBodette <jr3python@gmail.com>
2022-11-27 22:27:37 -0500

    attempt at staggerChildren (it does not work)
    gonna use delay instead... keeping this as a commit in case I want to look at it later

--- src/Animation/Box1.jsx
+++ src/Animation/Box1.jsx
@@ -8,10 +8,18 @@ export default function Box1() {
         end: { y: 500, opacity: 1 },
       }
 
+      const cubeOfCubes = {
+        hidden: { opacity: 0 },
+        visible:{
+            opacity: 1,
+        transition: {staggerChildren: 1}
+        }
+    }
+
 
 
 return(
-    <motion.div className="box-container" >
+    <motion.div className="box-container" variants={cubeOfCubes} initial="hidden" animate="visible">
         <motion.div className="box-blue" initial="start" animate="end" variants={littleCube} transition={{ duration: 2 }}>
         </motion.div>
         <motion.div className="box-orange" initial="start" animate="end" variants={littleCube} transition={{ duration: 2 }}>
