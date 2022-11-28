7f91f2bad63d36ee9c023e961be06075edb3b0ff
JulieBodette <jr3python@gmail.com>
2022-11-27 22:03:58 -0500

    added variants
    littleCube is a variant that defines the basic "cube move down" animation
    also put the cubes in a flexbox
    also made classes (ie box-orange) for each box color

--- src/Animation/Box1.jsx
+++ src/Animation/Box1.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import {motion} from "framer-motion"
 
 export default function Box1() {
+
+    const littleCube = {
+        start: { opacity: 0 },
+        end: { y: 500, opacity: 1 },
+      }
+
 return(
     <div className="box-container">
-        <motion.div className="box" 
+        <motion.div className="box-blue" 
         initial ={{opacity: 0}}
         animate={{ y: 500, opacity: 1 }} 
         transition={{ duration: 2 }}>
+
+        </motion.div>
+        <motion.div className="box-orange" initial="start" animate="end" variants={littleCube}>
 
         </motion.div>
 
