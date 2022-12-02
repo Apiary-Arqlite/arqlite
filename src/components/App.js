e011b716585c8ba1e8a9c02f7570fadd4d26fc07
nuriya <nuriya_a@yahoo.com>
2022-12-01 23:06:49 -0500

    wip fix styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,23 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import MeetingCard from "./MeetingCard";
+import buildingImg from '../images/building-image.png';
 
 /* -------------------------------------------------------------------------- */
 /*                                function App                                */
 /* -------------------------------------------------------------------------- */
 function App() { 
-
+const handleArrangeMeetingClick=()=>{
+  console.log("schedule meeting");
+}
   return (
     <div className="root">
       <div className="page">
         <Header />
         <Main/>
-        <MeetingCard></MeetingCard>
+        <MeetingCard
+        handleArrangeMeetingClick={handleArrangeMeetingClick}
+        img={buildingImg}></MeetingCard>
         <Footer />
       </div>
     </div>
--- src/components/MeetingCard.js
+++ src/components/MeetingCard.js
@@ -1,17 +1,33 @@
-import { Section } from "../components/Section";
-import buildingImg from "../images/building-image.png"
-function MeetingCard(){
-  return(
-   
+import {Section} from '../components/Section';
+import buildingImg from '../images/building-image.png';
+import downloadIcon from '../images/download-icon-white.png';
+function MeetingCard({handleArrangeMeetingClick, img}) {
+  return (
     <Section.MeetingCard>
-<div className="card__container">
-  <div className="card__text">
-    <p>Arrange a meeting</p>
-  </div>
-  <img className="card__img"src={buildingImg}/>
-</div>
+      <Section.MeetingCardContainer>
+        <Section.Header>Arrange a meeting</Section.Header>
+        <Section.CaptionLarge>
+          We will discuss your specific case and the details of our partnership
+        </Section.CaptionLarge>
+        <Section.MeetingCardAction>
+          <Section.MeetingCardButton
+            type="button"
+            onClick={handleArrangeMeetingClick}
+          >
+            Arrange a meeting
+          </Section.MeetingCardButton>
+          <Section.MeetingCardDownload>
+            Download our licensing Deck
+            <img
+              className="meetingCard__icon"
+              src={downloadIcon}
+              alt="download icon"
+            />
+          </Section.MeetingCardDownload>
+        </Section.MeetingCardAction>
+      </Section.MeetingCardContainer>
+      <img className="meetingCard__img" src={img} />
     </Section.MeetingCard>
-  
-  )
+  );
 }
 export default MeetingCard;
