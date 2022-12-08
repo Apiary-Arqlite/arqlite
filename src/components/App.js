21e43ee121ab5196b05aa9b155d28548746a69d3
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-12-07 17:16:35 -0800

    remove extra divs from App.js - styling covered with globalstyles

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,14 @@ import Footer from "./Footer";
 /* -------------------------------------------------------------------------- */
 function App() {
   return (
-    <div >
-      {/* <div className="page"> */}
+
       <div>
         <Header />
         <Main />
 
         <Footer />
       </div>
-    </div>
+ 
   );
 }
 
