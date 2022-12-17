23d934ffbf5c9a787fe6317b5d6e247d76c39f7f
jendoc <jaharrisnv@gmail.com>
2022-12-17 12:53:25 -0800

    feat: sticky navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,38 @@
 import "../blocks/navbar.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Section } from "./Section";
+import { debounce } from "../utils/helpers";
 
-function NavBar({ handleArrangeMeetingClick}) {
+function NavBar({ handleArrangeMeetingClick }) {
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
     <Section className="navbar">
-      <div className="navbar__links">
+      <div
+        className="navbar__links"
+      >
         <Section.NavLink
           className="navbar__link navbar__link-calc"
           href="#calculator"
@@ -27,7 +53,7 @@ function NavBar({ handleArrangeMeetingClick}) {
           className="navbar__link navbar__link-factory"
           href="#recycle"
         >
-          <Section.FactoryIcon className="navbar__link-icon"/>
+          <Section.FactoryIcon className="navbar__link-icon" />
           <h4 className="navbar__link-caption">
             For plastic producers and recyclers
           </h4>
