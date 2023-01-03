bd5bcaad3e91dc08cb289d68f797147c460d1f95
JulieBodette <jr3python@gmail.com>
2023-01-02 20:30:34 -0500

    formatting header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import "../blocks/header.css";
-import { Section } from "./Section";
-import arqliteLogo2 from "../images/arqlite-logo-2.svg";
-import Box1 from "./Box1.jsx";
-import "../blocks/Box1.css";
-import NavBar from "./NavBar";
+import React, { useState } from 'react';
+import '../blocks/header.css';
+import { Section } from './Section';
+import arqliteLogo2 from '../images/arqlite-logo-2.svg';
+import Box1 from './Box1.jsx';
+import '../blocks/Box1.css';
+import NavBar from './NavBar';
 
 function Header({ handleArrangeMeetingClick }) {
   const [mobileNavOpened, setMobileNavOpened] = useState(false);
@@ -13,7 +13,7 @@ function Header({ handleArrangeMeetingClick }) {
   return (
     <Section.Header className="header" id="header">
       <div className="header__wrapper">
-        <div className={mobileNavOpened ? "header__mobile-nav_bg" : ""}>
+        <div className={mobileNavOpened ? 'header__mobile-nav_bg' : ''}>
           <div className="header__mobile-nav" id="mobile-nav">
             <a className="header__logo-path" href="#header">
               <img
@@ -25,28 +25,26 @@ function Header({ handleArrangeMeetingClick }) {
             <button
               className={
                 mobileNavOpened
-                  ? "header__navbar-btn_opened"
-                  : "header__navbar-btn_closed"
+                  ? 'header__navbar-btn_opened'
+                  : 'header__navbar-btn_closed'
               }
               onClick={toggleMobileNav}
             />
             <div
               className={
                 mobileNavOpened
-                  ? "header__mobile-nav_opened"
-                  : "header__mobile-nav_closed"
+                  ? 'header__mobile-nav_opened'
+                  : 'header__mobile-nav_closed'
               }
             >
               <NavBar
                 handleArrangeMeetingClick={handleArrangeMeetingClick}
-                id={"navbar_mobile"}
+                id={'navbar_mobile'}
               />
             </div>
           </div>
         </div>
-        <h1 className="header__title">
-          Recycle<br></br>the unrecyclable
-        </h1>
+        <h1 className="header__title">Recycle the unrecyclable</h1>
         <Section.CaptionLarge className="header__caption">
           We set up a recycling line of hard-to-recycle plastic waste for your
           company. From zero to production-ready.
