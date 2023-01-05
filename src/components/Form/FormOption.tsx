514c15f24d785018f6de141c5239a793041d06b2
Ty Maraist <tcmaraist@gmail.com>
2023-01-05 10:39:15 -0600

    housekeeping. remove unnecassary css declarations and comments

--- src/components/Form/FormOption.tsx
+++ src/components/Form/FormOption.tsx
@@ -1,39 +1,6 @@
 import React from 'react';
 import { FormOptionProps } from './FormTypes';
-import styled from 'styled-components';
-
-export const StyledFormOption = styled.option`
-  margin: 0 auto;
-  padding-left: 20px;
-
-  height: 46px;
-
-  list-style: none;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 22px;
-  color: #54585b;
-
-  &:hover {
-    background-color: #f3f3f3;
-    cursor: pointer;
-  }
-`;
 
 export default function FormOption(props: FormOptionProps) {
-  return (
-    <StyledFormOption
-      value={props.value}
-      onClick={() => {
-        console.log(props.value);
-        // set isOpen to !isOpen
-      }}
-    >
-      $ {props.value}
-    </StyledFormOption>
-  );
+  return <option value={props.value}>$ {props.value}</option>;
 }
